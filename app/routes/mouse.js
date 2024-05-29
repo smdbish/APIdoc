eb0b5c99a08200b49878f762cdb6c47fbc39ccb6
mkrajimca <54492113+mkrajimca@users.noreply.github.com>
2024-05-29 21:36:53 +0000

    SM-1533 Make the studyInfo field of the Get Animal API an array

--- app/routes/mouse.js
+++ app/routes/mouse.js
@@ -236,7 +236,6 @@
  * 
  */
 
-
  /**
  * @apiDefine AnimalResp
  * @apiSuccess {Object[]} respObj Animal Objects (Array)
@@ -269,7 +268,6 @@
  * @apiSuccess {DateTime} respObj.createdDateTime 
  * @apiSuccess {String} respObj.altId alt ID / ear mark 
  * @apiSuccess {String} respObj.genotype 
- * // @apiSuccess {String} respObj.sampleMass
  * @apiSuccess {String} respObj.studyInfo
  * @apiSuccess {String} respObj.cageSid 
  * @apiSuccess {String} respObj.cageTag
@@ -361,6 +359,106 @@
  *            "currentPage": 1,
  *            "totalNum": 2
  *		}
+ * 
+ */
+
+ /**
+ * @apiDefine AnimalResp_v1_3
+ * @apiSuccess {Object[]} respObj Animal Objects (Array)
+ * @apiSuccess {Number} respObj.id Animal Id
+ * @apiSuccess {String} respObj.ownerUser Owner name
+ * @apiSuccess {String} respObj.creatorUser Creator name
+ * @apiSuccess {Number} respObj.guiSid Animal SID
+ * @apiSuccess {String} respObj.sex Sex
+ * @apiSuccess {Date} respObj.birthDate Birth date
+ * @apiSuccess {Number=0,1,2,3,4,5,6} respObj.state Animal state 0-"Ended", 1-"Stock",2-"Mating",3-"Weanling",4-"Deleted",5-"Pup", 6-"Ordered"
+ * @apiSuccess {String} respObj.physicalTag Physical tag
+ * @apiSuccess {String} respObj.litterSid Litter SID
+ * @apiSuccess {String} respObj.strain “Strain/Mouseline name”
+ * @apiSuccess {String} respObj.protocol Protocol name
+ * @apiSuccess {String} respObj.background Background
+ * @apiSuccess {String} respObj.generation Generation
+ * @apiSuccess {Date} respObj.receivedDate Received date
+ * @apiSuccess {Date} respObj.endDate End date 
+ * @apiSuccess {Date} respObj.weanDate Wean date 
+ * @apiSuccess {Date} respObj.tailTagDate  TailTag date
+ * @apiSuccess {Date} respObj.matureDate 
+ * @apiSuccess {String} respObj.phenotype 
+ * @apiSuccess {String} respObj.comment 
+ * @apiSuccess {String} respObj.sourceSupplier 
+ * @apiSuccess {String} respObj.status 
+ * @apiSuccess {String} respObj.category 
+ * @apiSuccess {String} respObj.notice 
+ * @apiSuccess {String} respObj.endType 
+ * @apiSuccess {String} respObj.endReason 
+ * @apiSuccess {DateTime} respObj.createdDateTime 
+ * @apiSuccess {String} respObj.altId alt ID / ear mark 
+ * @apiSuccess {String} respObj.genotype 
+ * @apiSuccess {String} respObj.studyInfo
+ * @apiSuccess {String} respObj.cageSid 
+ * @apiSuccess {String} respObj.cageTag
+ * @apiSuccess {String} respObj.cageBarcode
+ * @apiSuccess {DateTime} respObj.lastUpdatedDateTime
+ * @apiSuccess {Number} totalPage
+ * @apiSuccess {Number} currentPage
+ * @apiSuccess {Number} totalNum
+ * @apiSuccessExample {json} Success-Response:
+ *     {
+ *   "messageCode": "200",
+ *   "messageDesc": "SUCCESS",
+ *   "respObj": [
+ *       {
+ *           "id": 914069,
+ *           "ownerUser": "Raji Test",
+ *           "creatorUser": "Raji Test",
+ *           "guiSid": 2281,
+ *           "sex": 1,
+ *           "birthDate": "2014-11-07",
+ *           "state": 2,
+ *           "physicalTag": "1304",
+ *           "litterSid": "153.1",
+ *           "strain": "hsf1",
+ *           "protocol": null,
+ *           "background": null,
+ *           "generation": null,
+ *           "receivedDate": null,
+ *           "endDate": null,
+ *           "weanDate": "2014-11-28",
+ *           "tailTagDate": null,
+ *           "matureDate": "2014-12-19",
+ *           "phenotype": null,
+ *           "comment": null,
+ *           "sourceSupplier": "Jax",
+ *           "status": null,
+ *           "category": "c2",
+ *           "notice": "Reserved for study_512",
+ *           "endType": null,
+ *           "endReason": null,
+ *           "createdDateTime": "2015-04-17T08:32:28Z",
+ *           "altId": null,
+ *           "genotype": "hsf1a(-/-)",
+ *           "lastUpdatedDateTime": "2023-11-29T13:17:11Z",
+ *           "cageSid": "1877",
+ *           "cageTag": null,
+ *           "cageBarcode": null,
+ *           "studyInfo": [
+ *               {
+ *                   "code": "Study_302 B",
+ *                   "group": "Without Group",
+ *                   "status": "Inactive"
+ *               },
+ *               {
+ *                   "code": "study_231127",
+ *                   "group": "group1",
+ *                   "status": "Active"
+ *               }
+ *           ]
+ *       }
+ *   ],
+ *   "totalPage": 1,
+ *   "currentPage": 1,
+ *   "totalNum": 1
+ *}
  * 
  */
 
@@ -519,6 +617,64 @@
  * @apiSampleRequest /v1.2/animal 
  */
 
+/**
+ * @apiVersion 1.3.0
+ * @api {get} /external/v1.3/animal Get Animal
+ * @apiName getAnimal 
+ * @apiGroup Animal
+ * @apiDescription The Get Animal api call is used to pull information on one, or a group of animals. The API user must have View access on those records.
+ * 
+ * @apiPermission Normal user. The customer should open API access.
+ * 
+ * @apiExample {curl} Example usage:
+ *     curl -i "https://softmouse.net/external/v1.2/animal?ownerUser_name=test&animalSID=2278&animalSID=2279"  \
+ *          -H "Content-Type:application/json"  \
+ *          -H "Authorization:SMDB-oauthtoken CWKw%ij7h_&8c.VN0XDXwT#WqW!z.A70EmMI" \
+ *          -X GET
+ * 
+ * @apiExample {curl} Example usage:
+ *     curl -i "https://softmouse.net/external/v1.2/animal?studyCode=study2021-01-04-004&studyCode=study2021-01-05-004" \
+ *          -H "Content-Type:application/json"  \
+ *          -H "Authorization:SMDB-oauthtoken CWKw%ij7h_&8c.VN0XDXwT#WqW!z.A70EmMI" \
+ *          -X GET
+ * 
+ * @apiExample {curl} Example usage:
+ *     curl -i "https://softmouse.net/external/v1.2/animal?sinceDateTime=2021-02-08T00:00:01Z&ownerUser_name=David Yu&ownerUser_name=Meabc Lu"  \
+ *          -H "Content-Type:application/json" \
+ *          -H "Authorization:SMDB-oauthtoken CWKw%ij7h_&8c.VN0XDXwT#WqW!z.A70EmMI" \
+ *          -X GET
+ * 
+ * @apiExample {curl} Example usage:
+ *     curl -i "https://softmouse.net/external/v1.2/animal?sinceDateTime=2023-02-08T00:00:01Z&pageNo=2"  \
+ *          -H "Content-Type:application/json"  \
+ *          -H "Authorization:SMDB-oauthtoken CWKw%ij7h_&8c.VN0XDXwT#WqW!z.A70EmMI" \
+ *          -X GET
+ * 
+ * @apiExample {curl} Example usage:
+ *     curl -i "https://softmouse.net/external/v1.2/animal?sinceDateTime=2023-01-08T00:00:01Z&state=ENDED&state=ORDERED"  \
+ *          -H "Content-Type:application/json"  \
+ *          -H "Authorization:SMDB-oauthtoken CWKw%ij7h_&8c.VN0XDXwT#WqW!z.A70EmMI" \
+ *          -X GET
+ * 
+ *      
+ * @apiUse RequestHeader
+ * @apiParam {Number[]} [AnimalIds] Animal Id array
+ * @apiParam {String[]} [ownerUser_name] Owner login name or full name
+ * @apiParam {Number[]} [ownerUser_id] Owner user id
+ * @apiParam {Number[]} [animalSID] Animal SID array
+ * @apiParam {String[]="ENDED","STOCK", "MATING", "WEANLING", "PUP","ORDERED"} [state] Animal State
+ * @apiParam {String[]} [studyCode] Study Code
+ * @apiParam {String="ACTIVE"} [studyState] Study State
+ * @apiParam {DateTime} [sinceDateTime] Return all changed mice after this datetime
+ * @apiParam {Number} [numberPerPage] Number of Records per Page, default is 200
+ * @apiParam {Number} [pageNo] Page No
+ * 
+ * @apiUse ResponseFormat
+ * @apiUse AnimalResp_v1_3
+ *
+ * @apiUse TokenErrorResp
+ * @apiSampleRequest /v1.2/animal 
+ */
 
   
  /**
